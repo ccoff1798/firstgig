86e2191d9f0b4cf69c8db9dd3dc667788459ae0a
Cody <ccoff1798@gmail.com>
2024-02-12 14:30:53 -0700

    Put header icon in div, formatted a bit not done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import ElkLogo from '../assets/images/ElkLogo.svg';
 const Header = () => {
   return (
     <div>
-    <header className="header">
-      <img src={ElkLogo} alt="Logo" className='justify-left scale-down' />
+    <header className="header height-cap">
+      <div className = "justify-left">
+      <img src={ElkLogo} alt="Logo" className="height-cap" />
+      </div>
       <h1 className = "header-title">Blue Mesa Point</h1>
     </header>
     </div>
