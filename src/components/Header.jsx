a1ad71a00f4a7aee8f5eb6dde59c829f946a0e55
Cody <ccoff1798@gmail.com>
2024-02-12 11:27:09 -0700

    logo in works, still bad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-
+import ElkLogo from '../assets/images/ElkLogo.svg';
 const Header = () => {
   return (
     <div>
     <header className="header">
+      <img src={ElkLogo} alt="Logo" className='justify-left scale-down' />
       <h1 className = "header-title">Blue Mesa Point</h1>
     </header>
     </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -5,15 +5,10 @@ const About = () => {
 
   return (
     <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Home Page</h2>
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-      </div>
+      <h1>Who are we?</h1>
+      <p>We are Colorado residents who want to help others enjoy the beauty and serenity that our gorgeous parks offer. Filler Text Until
+        I have more information!
+      </p>
     </div>
   );
 };
