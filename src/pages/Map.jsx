cd98d243e9671fe2a5f7cbac04ba597aa89cb5db
Cody <ccoff1798@gmail.com>
2024-02-15 10:55:17 -0700

    updated map max width

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -4,7 +4,7 @@ export default function Map() {
         <div>
             <h1 className='center'>Map of Blue Mesa Reservoir and Curecanti National Recreation Area</h1>
             <iframe className = 'center' src={CurecantiMap}
-            width='110%' height='500rem'>
+            width='80%' height='500rem'>
             </iframe>
 
             <h5>Map Provided by the U.S National Parks Service</h5>
